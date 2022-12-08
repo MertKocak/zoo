fc0b8808b1df00221a8d20887dbf59845e58a367
merttkocak <mertkocak.2811@gmail.com>
2022-12-08 18:11:13 +0300

    uygulama yenilendi

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -15,7 +15,7 @@ class _HomePageState extends State<HomePage> {
     var height = size.height;
 
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Color.fromARGB(255, 255, 244, 244),
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -23,7 +23,9 @@ class _HomePageState extends State<HomePage> {
           style:
               TextStyle(fontSize: 50, color: Colors.white, fontFamily: "quick"),
         ),
-        backgroundColor: Color.fromRGBO(49, 27, 146, 1),
+        backgroundColor: Color.fromARGB(255, 255, 63, 117),
+        bottomOpacity: 0.0,
+        elevation: 0.0,
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -31,28 +33,28 @@ class _HomePageState extends State<HomePage> {
           Column(
             children: [
               Container(
-                height: height / 30,
+                height: height / 20,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/inek.png",
-                  Color.fromARGB(192, 240, 98, 145), "audio/inek.mp3"),
+              animal("assets/images/inek.png", Colors.pink.shade300,
+                  "audio/inek.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/aslan.png",
-                  Color.fromARGB(184, 255, 168, 38), "audio/aslan.mp3"),
+              animal("assets/images/aslan.png", Colors.brown.shade400,
+                  "audio/aslan.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/at.png", Color.fromARGB(182, 93, 64, 55),
+              animal("assets/images/ari.png", Colors.orange.shade300,
                   "audio/at.mp3"),
               Container(
                 height: height / 40,
@@ -61,26 +63,26 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               animal("assets/images/karga.png",
-                  Color.fromARGB(179, 158, 158, 158), "audio/karga.mp3"),
+                  Color.fromARGB(255, 66, 212, 154), "audio/karga.mp3"),
             ],
           ),
           Column(
             children: [
               Container(
-                height: height / 30,
+                height: height / 20,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/ayi.png", Color.fromARGB(173, 153, 52, 16),
-                  "audio/ayi.mp3"),
+              animal("assets/images/kus.png",
+                  Color.fromARGB(255, 250, 188, 165), "audio/ayi.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/kedi.png", Color.fromARGB(181, 239, 83, 80),
+              animal("assets/images/kedi.png", Colors.purple.shade400,
                   "audio/kedi.mp3"),
               Container(
                 height: height / 40,
@@ -89,7 +91,7 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               animal("assets/images/kopek.png",
-                  Color.fromARGB(176, 245, 127, 23), "audio/kopek.mp3"),
+                  Color.fromARGB(255, 231, 104, 0), "audio/kopek.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
@@ -97,42 +99,42 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
               animal("assets/images/koyun.png",
-                  Color.fromARGB(173, 170, 71, 188), "audio/koyun.mp3"),
+                  Color.fromARGB(255, 203, 17, 235), "audio/koyun.mp3"),
             ],
           ),
           Column(
             children: [
               Container(
-                height: height / 30,
+                height: height / 20,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
               animal("assets/images/kurbaga.png",
-                  Color.fromARGB(174, 46, 125, 50), "audio/kurba.mp3"),
+                  Color.fromARGB(255, 47, 189, 52), "audio/kurba.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/kurt.png",
-                  Color.fromARGB(174, 189, 189, 189), "audio/kurt.mp3"),
+              animal("assets/images/ordek.png",
+                  Color.fromARGB(255, 2, 141, 255), "audio/kurt.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/maymun.png",
-                  Color.fromARGB(182, 255, 168, 38), "audio/maymun.mp3"),
+              animal("assets/images/maymun.png", Colors.orange,
+                  "audio/maymun.mp3"),
               Container(
                 height: height / 40,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                 ),
               ),
-              animal("assets/images/fil.png", Color.fromARGB(177, 21, 101, 192),
+              animal("assets/images/fil.png", Color.fromARGB(255, 21, 101, 192),
                   "audio/fil.mp3"),
             ],
           ),
@@ -144,21 +146,19 @@ class _HomePageState extends State<HomePage> {
   animal(String image, Color bgcolor, String audio) {
     return InkWell(
       borderRadius: BorderRadius.circular(20),
-      splashColor: bgcolor,
       onTap: (() {
         final player = AudioCache();
         player.play(audio);
       }),
       child: Container(
         height: 140,
+        width: 120,
         decoration: BoxDecoration(
           color: bgcolor,
           borderRadius: BorderRadius.circular(20),
         ),
         child: Image.asset(
           image,
-          width: 120,
-          height: 140,
         ),
       ),
     );
